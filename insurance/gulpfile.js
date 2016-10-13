07060b3a666c4a646d7dc60d8773ba2b58ada9d0
kay <chenyuanluck@163.com>
2016-10-13 16:14:17 +0800

    Update gulpfile.js

--- insurance/gulpfile.js
+++ insurance/gulpfile.js
@@ -28,6 +28,26 @@ var concat = require('gulp-concat');
 // gulp-exec
 var exec = require('gulp-exec');
 
+
+
+
+// 文件引入
+var fileInclude = require('gulp-file-include');
+// 配置文件
+var config = require('./gulpConfig.json');
+// 文本替换
+var replace = require('gulp-replace');
+// 重命名
+var rename = require('gulp-rename');
+// 合并文件
+var concat = require('gulp-concat');
+// gulp-exec
+var exec = require('gulp-exec');
+
+
+
+
+
 /**
  * 清理build目录
  */
